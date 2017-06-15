ff1c7054f7bef62ad49e9e51fcdeb2e9145e4caf
Lucy Kull <lucy.kull@cerner.com>
2017-06-15 14:58:54 -0500

    Added new tables

--- HashtagHealth-DB.py
+++ HashtagHealth-DB.py
@@ -33,8 +33,9 @@ def main():
 
     sql_create_main_table = """ CREATE TABLE IF NOT EXISTS main (
                                         id INTEGER PRIMARY KEY,
+                                        Tweet_ID INT NOT NULL,
                                         Location INT NOT NULL,
-                                        Time INT NOT NULL,
+                                        DateTime INT NOT NULL,
                                         Profile_location INT,
                                         Full_Tweet VARCHAR(400) NOT NULL,
                                         Hashtags TEXT,
@@ -42,11 +43,74 @@ def main():
                                         BuzzWords TEXT
                                     ); """
 
+    sql_create_region_table = """ CREATE TABLE IF NOT EXISTS region (
+                                        id INTEGER PRIMARY KEY,
+                                        Locale INT NOT NULL,
+                                        Country VARCHAR(25),
+                                        State VARCHAR (10),
+                                        City VARCHAR(25)
+                                        ); """
+
+    sql_create_tweets_table = """ CREATE TABLE IF NOT EXISTS tweets (
+                                        id INTEGER PRIMARY KEY,
+                                        Tweet_ID INT NOT NULL,
+                                        Tweet_Text TEXT NOT NULL,
+                                        Topic VARCHAR(25),
+                                        DateTime INT NOT NULL,
+                                        Bio_Location VARCHAR(25),
+                                        Tweet_Location VARCHAR(25)
+                                        ); """
+
+    sql_create_users_table = """ CREATE TABLE IF NOT EXISTS users (
+                                        id INTEGER PRIMARY KEY,
+                                        User_ID VARCHAR(25),
+                                        Twitter_Handle VARCHAR(25),
+                                        Region VARCHAR(25)
+                                        ); """
+
+    sql_create_tweeted_by_table = """ CREATE TABLE IF NOT EXISTS tweeted_by (
+                                        id INTEGER PRIMARY KEY,
+                                        User_ID VARCHAR(25),
+                                        Tweet_ID INT NOT NULL
+                                        ); """
+
+    sql_create_trends_table = """ CREATE TABLE IF NOT EXISTS trends (
+                                        id INTEGER PRIMARY KEY,
+                                        Trend_Topic VARCHAR(25),
+                                        DateTime INT NOT NULL,
+                                        Region VARCHAR(25)
+                                        ); """
+
+    sql_create_categories_table = """ CREATE TABLE IF NOT EXISTS categories (
+                                        id INTEGER PRIMARY KEY,
+                                        Category VARCHAR(25)
+                                        ); """
+
+    sql_create_buzzwords_table = """ CREATE TABLE IF NOT EXISTS buzzwords (
+                                        id INTEGER PRIMARY KEY,
+                                        Buzzword VARCHAR(25)
+                                        ); """
+
+    sql_create_topics_table = """ CREATE TABLE IF NOT EXISTS topics (
+                                        id INTEGER PRIMARY KEY,
+                                        Category VARCHAR(25),
+                                        Buzzword VARCHAR(25)
+                                        ); """
+
     # create a database connection
     conn = create_connection(database)
     if conn is not None:
         # create table
         create_table(conn, sql_create_main_table)
+        create_table(conn, sql_create_region_table)
+        create_table(conn, sql_create_tweets_table)
+        create_table(conn, sql_create_users_table)
+        create_table(conn, sql_create_tweeted_by_table)
+        create_table(conn, sql_create_trends_table)
+        create_table(conn, sql_create_categories_table)
+        create_table(conn, sql_create_buzzwords_table)
+        create_table(conn, sql_create_topics_table)
+
     else:
         print("Error! cannot create the database connection.")
 
