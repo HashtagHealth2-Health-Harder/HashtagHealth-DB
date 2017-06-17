7056b5dcbeeeed7d782a95e2b9fc5c91a00fc98d
thatsmysky <lucy.r.kull@gmail.com>
2017-06-17 13:37:56 -0500

    Added functions for parse and add Gabby's object to DB

--- PopulateDatabase-Gabby.py
+++ PopulateDatabase-Gabby.py
@@ -1,18 +1,11 @@
 import HashtagHealth_DB
-import exceptions
 import pickle
 
 
-def load_file(file_path, file_name):
-    try:
-        tweets = []
-        from file_path import file_name
-        with open(file_name) as f:
-            for line in f:
-                tweets.append(pickle.decode_long(line))
-        return tweets
-    except exceptions as e:
-        print(e)
+def load_file(file_name):
+    tweets = pickle.load(open(file_name, 'rb'))
+    return tweets
+
 
 def main():
     # this data needs to go into the database
@@ -20,3 +13,18 @@ def main():
     # we need it in tweets
     # we need it in users
     # we need it in all related tables
+    tweets = load_file("2017-06-16_2004081497643448938.pickle")
+    #print(tweets.id)
+    #print(tweets.place.full_name)
+    #print(tweets.created_at)
+    #print(tweets.user.location)
+    #print(tweets.extended_tweet["full_text"])
+    #print(tweets.entities["hashtags"])
+    #print(tweets.entities["user_mentions"])
+    HashtagHealth_DB.insert_tweet(tweets.id, tweets.place.full_name, tweets.created_at, tweets.user.location,
+                                  tweets.extended_tweet["full_text"], tweets.entities["hashtags"],
+                                  tweets.entities["user_mentions"])
+    print("Table created?")
+
+if __name__ == '__main__':
+    main()
